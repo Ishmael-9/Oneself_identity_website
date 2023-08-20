3c0408b432390b9a41bd7c80f4fa96e6030b1579
Ishmael-9 <86658941+Ishmael-9@users.noreply.github.com>
2023-08-20 10:10:33 +0200

    Add files via upload

---
+++ per.js
@@ -0,0 +1,8 @@
+const name = ["I", "S", "H", "M","A", "E","L"
+];
+/*
+let alpha-preview = document.getElementById("anime-box");
+*/
+const box = body.getElementById("anime-box");
+
+void name-animation()
